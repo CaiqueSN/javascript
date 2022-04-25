12aad67e21dabc038f7273d3039ebd0ffa34c23d
Caique Nascimento <102535816+CaiqueSN@users.noreply.github.com>
2022-04-25 15:56:13 -0300

    condição

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+if (vel > 60) {
+    console.log('Multado')
+}
+console.log ('Sempre utilize o cinto de segurança')
---
+++ aula12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 22
+console.log(`Você tem ${idade} anos.`)
+if (idade >= 18 && idade < 67) {
+    console.log('Vota')
+} else if (idade >= 16) {
+        console.log('Voto não obrigatório')
+    } else {
+        console.log('Não vota')
+    }
