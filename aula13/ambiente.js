25bccda428268b8dfd09f1efe35a746564c87498
Caique Nascimento <102535816+CaiqueSN@users.noreply.github.com>
2022-05-16 14:04:20 -0300

    repet

---
+++ aula13/ambiente.js
@@ -0,0 +1,21 @@
+console.log('começo')
+for (var c =1; c < 6; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('fim')
+
+
+/* var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c < 16)
+*/
+
+
+/* var c = 1
+while(c < 6) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
