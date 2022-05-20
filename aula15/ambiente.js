fa77fe4a5c56eb34c11fc5ae32702b0254710beb
Caique Nascimento <102535816+CaiqueSN@users.noreply.github.com>
2022-05-20 15:28:01 -0300

    array

---
+++ aula15/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5,8,2,9,3]
+console.log(`Mosso vetor é ${num}`)
+
+num[3]= 6
+console.log(num)
+
+num.push(7)
+console.log(num)
+
+num.length
+console.log(`O primeiro vetor é ${num[0]}`)
+
+num.sort()
+console.log(num)
+
+let pos = num.indexOf(8)
+console.log(pos)
